1e67c2fbcb55a121b099be85b0d95e5dddbc27de
Vaibhav-dev-afk <vaibhavdhiman265@gmail.com>
2026-06-03 17:22:59 +0530

    update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect('mongodb+srv://vaibhavdhiman265_db_user:XdRJm0A6AniMinEU@taskmanager.gmkzjji.mongodb.net/DevCollab?retryWrites=true&w=majority')
+    .then(() => console.log('Successfully connected to Mongodb Atlas!'))
+    .catch((error) => console.log('Database connection is failed:', error));
 
 app.get('/', (req,res) => {
     res.send('TaskManager is working!');
